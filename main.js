24d2eb16fbc47cbd6f5fc5e7f98f977abef35f15
Santiago <santiagodmaldon@gmail.com>
2022-02-10 13:11:19 -0400

    First Commit

---
+++ main.js
@@ -0,0 +1,12 @@
+document.querySelector('.menu-btn').addEventListener('click',()=>{
+    document.querySelector('.nav-menu').classList.toggle('show');
+});
+
+ScrollReveal().reveal('.header',);
+ScrollReveal().reveal('.news-cards',{delay:500});
+ScrollReveal().reveal('.banner-1',{delay:500});
+ScrollReveal().reveal('.banner-2',{delay:500});
+ScrollReveal().reveal('.social',{delay:500});
+ScrollReveal().reveal('.footer-links',{delay:500});
+
+
